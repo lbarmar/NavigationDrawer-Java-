0d776e552c83a89c783a4dc3d176600a5fdca8a6
Lindsay <lin.barrero.martinez@gmail.com>
2025-01-15 14:15:52 +0100

    Added button with sound

--- app/src/main/java/dam/pmdm/navigationdrawerjava/HomeFragment.java
+++ app/src/main/java/dam/pmdm/navigationdrawerjava/HomeFragment.java
@@ -1,10 +1,14 @@
 package dam.pmdm.navigationdrawerjava;
 
+import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
+
 import dam.pmdm.navigationdrawerjava.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
@@ -16,7 +20,37 @@ public class HomeFragment extends Fragment {
         // Inicializar binding
         binding = FragmentHomeBinding.inflate(inflater, container, false);
 
+        binding.button.setOnClickListener(this::OnClickButton);
+
         // Retornar la vista raíz
         return binding.getRoot();
+    }
+
+    private void OnClickButton(View view) {
+//        // Usar SoundPool (comentado como en Kotlin)
+//         SoundPool soundPool = new SoundPool.Builder().setMaxStreams(1).build();
+//         int soundId = soundPool.load(requireContext(), R.raw.pulse, 1);
+//
+//         soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+//             @Override
+//             public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+//                 if (status == 0) {  // Carga exitosa
+//                     soundPool.play(soundId, 1f, 1f, 0, 0, 1f);
+//                 }
+//             }
+//         });
+
+//        Usar MediaPlayer
+        MediaPlayer mediaPlayer = MediaPlayer.create(getContext(), R.raw.pulse);
+        mediaPlayer.start();
+
+//        Establecer el listener para cuando la reproducción termine
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                // Liberar el MediaPlayer cuando la reproducción termine
+                mp.release();
+            }
+        });
     }
 }
